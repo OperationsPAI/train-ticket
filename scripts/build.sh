99a0d7d4bb18c68c4bbd4edc7d5047960f913731
rainystevn1 <rainystevn1@gmail.com>
2026-02-10 10:52:36 +0800

    add release manually

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 for dir in $(find . -maxdepth 2 -type f -name Dockerfile -exec dirname {} \;); do
   echo "Building Docker image in directory: $dir"
   IMAGE_NAME=$(basename "$dir")
-  docker build -t docker.io/opspai/$IMAGE_NAME:latest "$dir"
-  docker push docker.io/opspai/$IMAGE_NAME:latest
+  docker build -t pair-diag-cn-guangzhou.cr.volces.com/pair/$IMAGE_NAME:v1.2.9 "$dir"
+  docker push pair-diag-cn-guangzhou.cr.volces.com/pair/$IMAGE_NAME:v1.2.9
  done
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,44 @@
 set -e
 
 CHART_DIR="manifests/helm/trainticket"
-REPO_NAME="train-ticket"
-REPO_URL="https://lgu-se-internal.github.io/train-ticket"
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+    GIT_REMOTE=$(git config --get remote.origin.url 2>/dev/null || echo "")
+    if [[ $GIT_REMOTE =~ github.com[:/]([^/]+)/([^/.]+) ]]; then
+        ORG_NAME="${BASH_REMATCH[1]}"
+        REPO_NAME="${BASH_REMATCH[2]}"
+    else
+        ORG_NAME="your-org-name"
+        REPO_NAME="train-ticket"
+        echo "Warning: Using default ORG_NAME and REPO_NAME"
+    fi
+else
+    ORG_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f1 | tr '[:upper:]' '[:lower:]')
+    REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2 | tr '[:upper:]' '[:lower:]')
+fi
+
+REPO_URL="https://${ORG_NAME}.github.io/${REPO_NAME}"
+
+echo "Target Repo URL: $REPO_URL"
+
+if ! command -v yq &> /dev/null; then
+    echo "yq not found, installing..."
+    YQ_VERSION="v4.44.1"
+    YQ_BINARY="yq_linux_amd64"
+    wget -qO /tmp/yq "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}"
+    chmod +x /tmp/yq
+
+    if sudo -n mv /tmp/yq /usr/local/bin/yq 2>/dev/null; then
+        echo "yq installed to /usr/local/bin/yq"
+    else
+        mv /tmp/yq ./yq
+        export PATH="$PWD:$PATH"
+        echo "yq installed to current directory"
+    fi
+    echo "yq installed successfully"
+fi
+
+yq eval ".dependencies[] |= select(.name == \"loadgenerator\").repository = \"https://${ORG_NAME}.github.io/loadgenerator\"" -i $CHART_DIR/Chart.yaml
 
 helm dependency update $CHART_DIR
 
